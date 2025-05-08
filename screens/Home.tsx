826d3a9dc6d3394f3e93567a317573caf86b2725
Hrishikesh <hrishikesh.jadhav@anveshak.com>
2025-05-08 17:55:44 +0530

    optimized added usecallback v1.3

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,35 +1,42 @@
-import React, {useState} from 'react';
-import {ScrollView, View, FlatList, TextInput} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {View, FlatList, TextInput} from 'react-native';
 import data from '../data.json';
 import Profile from '../components/Home/Profile';
 import styles from '../styles/homeStyles';
+
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const filteredData = data?.filter(item => {
-    return (
-      item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      item.address.toLowerCase().includes(searchTerm.toLowerCase())
+  const searchChange = useCallback((text: string) => {
+    setSearchTerm(text);
+  }, []);
+
+  const filterData = (term: string) => {
+    return data?.filter(
+      item =>
+        item.name.toLowerCase().includes(term.toLowerCase()) ||
+        item.address.toLowerCase().includes(term.toLowerCase()),
     );
-  });
+  };
+
   return (
-    <ScrollView>
+    <View style={styles.container}>
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.searchInput}
           placeholder="Search"
           placeholderTextColor="#000"
-          value={searchTerm}
           selectionColor="#000"
-          onChangeText={setSearchTerm}
+          onChangeText={searchChange}
         />
       </View>
       <FlatList
         style={styles.section}
-        data={filteredData}
+        data={filterData(searchTerm)}
         renderItem={({item}) => <Profile data={item} />}
         keyExtractor={item => item.id}
+        keyboardShouldPersistTaps="handled"
       />
-    </ScrollView>
+    </View>
   );
 }
